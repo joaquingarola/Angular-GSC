e018d01d890da25be2556ad4670fedadde946db4
Joaquin-Garola <joaquingarola75@gmail.com>
2022-11-01 15:22:19 -0300

    Solution ejercicio25

--- Angular/ejercicio23/src/app/create-event/create-event.component.ts
+++ Angular/ejercicio23/src/app/create-event/create-event.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { EventService } from '../event.service';
 
 @Component({
@@ -11,15 +11,20 @@ import { EventService } from '../event.service';
 export class CreateEventComponent implements OnInit {
 
   eventForm = this.fb.group({
-		name: [''],
-		date: [''],
-		time: [''],
+		name: ['', Validators.required],
+		date: ['', Validators.required],
+		time: ['', Validators.required],
 		location: this.fb.group({ 
-      address: [''], 
-      city: [''], 
-      country: ['']
+      address: ['', Validators.required], 
+      city: ['', Validators.required], 
+      country: ['', [Validators.pattern("^(.*?[A-Z]){2,}.*$"), , Validators.required]]
     })
 	});
+
+  constructor(private eventService: EventService, private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
 
   saveEvent() {
 		this.eventService.saveEvent(this.eventForm.value);
@@ -27,12 +32,15 @@ export class CreateEventComponent implements OnInit {
 		this.router.navigate(["/events"]);
 	}
 
-  constructor(private eventService: EventService, private router: Router, private fb: FormBuilder) { }
-
-  ngOnInit() {
-  }
-
   cancel() {
     this.router.navigate(['/events']);
+  }
+
+  isControlValid(control: string) {
+    return this.eventForm.get(control)!.errors?.['required'] && this.eventForm.get(control)?.touched
+  }
+
+  isCountryValid(control: string){
+    return this.eventForm.get(control)!.errors?.['pattern'] && this.eventForm.get(control)?.touched
   }
 }
